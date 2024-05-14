47f95e919abe6665d6d86dd417e319ec3c2fb9ad
root <root@Boammels.localdomain>
2024-05-14 22:43:31 +0800

    backend started

--- backend/server.py
+++ backend/server.py
@@ -5,9 +5,12 @@ from flask_cors import CORS
 from flask import Flask, request
 from echo import echo_upper
 import graph as GR
+import initialize from initialize
 
 APP = Flask(__name__)
 CORS(APP)
+
+building_list = initialize()
 
 
 @APP.route('/echo/post/', methods=['GET'])
