584f33c3f28d251db529a78a5771e4cce2962912
root <root@Boammels.localdomain>
2024-05-29 19:37:37 +0800

    uml diagram updated

--- backend/classes/buildingList.py
+++ backend/classes/buildingList.py
@@ -3,9 +3,9 @@ class Building_list:
     def __init__(self, building_list):
         self.building_list = building_list
 
-    def get_building_id (self, string):
+    def get_building_id (self, name):
         for building in self.building_list:
-            if building.english_name == string:
+            if building.english_name == name:
                 return building.building_id
             
     def get_building_info_by_facility_id(self, classroom_id):
